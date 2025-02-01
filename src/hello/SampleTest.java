84235ff198b0e5c4aff75170d0bb73d08470af9e
madhubalaji2811 <madhavan.balaji@hotmail.com>
2025-02-01 20:58:26 +0530

    this is the lastest commit from the indian tz

---
+++ src/hello/SampleTest.java
@@ -0,0 +1,24 @@
+package hello;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	
+@Test
+public void postJIRA() {
+		
+		System.out.println("Post JIRA 1");
+		System.out.println("Post JIRA 2");
+		System.out.println("Post JIRA 3");
+}
+
+
+
+@Test
+public void deleteTwitter() {
+	
+	System.out.println("this is the post to delete twitter");
+}
+
+}
